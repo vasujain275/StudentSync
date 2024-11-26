bd7519319b6bd4ec299906f658a0cfea1ced10e8
vasujain275 <vasujain275@gmail.com>
2024-11-26 09:53:47 +0530

    feat: Updated crud operation for notices

--- api/src/main/java/me/vasujain/controller/NoticeController.java
+++ api/src/main/java/me/vasujain/controller/NoticeController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/notices")
@@ -31,5 +32,29 @@ public class NoticeController {
     public ResponseEntity<Notice> createNotice(@RequestBody Notice notice) {
         Notice createdNotice = noticeService.createNotice(notice);
         return ResponseEntity.ok(createdNotice);
+    }
+
+    // PUT endpoint to update an existing notice by UUID query param
+    @PutMapping
+    public ResponseEntity<Notice> updateNotice(
+            @RequestParam("id") UUID uuid,
+            @RequestBody Notice noticeDetails
+    ) {
+        Notice updatedNotice = noticeService.updateNotice(uuid, noticeDetails);
+        return ResponseEntity.ok(updatedNotice);
+    }
+
+    // DELETE endpoint to delete a notice by UUID query param
+    @DeleteMapping
+    public ResponseEntity<Void> deleteNotice(@RequestParam("id") UUID uuid) {
+        noticeService.deleteNotice(uuid);
+        return ResponseEntity.noContent().build();
+    }
+
+    // GET endpoint to retrieve a single notice by UUID query param
+    @GetMapping("/details")
+    public ResponseEntity<Notice> getNoticeByUuid(@RequestParam("id") UUID uuid) {
+        Notice notice = noticeService.getNoticeByUuid(uuid);
+        return ResponseEntity.ok(notice);
     }
 }
