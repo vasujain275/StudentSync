06bc174a25894f2fd48cc3f6fe5f4e2e1cddd725
vasujain275 <vasujain275@gmail.com>
2024-12-22 17:29:23 +0530

    feat: new spring boot api added

---
+++ api/src/main/java/me/vasujain/studentsyncapi/StudentsyncapiApplication.java
@@ -0,0 +1,13 @@
+package me.vasujain.studentsyncapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsyncapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsyncapiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/me/vasujain/studentsyncapi/StudentsyncapiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.vasujain.studentsyncapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsyncapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
