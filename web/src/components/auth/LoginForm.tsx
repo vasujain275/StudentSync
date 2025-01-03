9a8982e572c5cb6e5417643fe6cb3c30150150b7
vasujain275 <vasujain275@gmail.com>
2025-01-03 23:28:58 +0530

    feat: connected api to login form

--- web/src/components/auth/LoginForm.tsx
+++ web/src/components/auth/LoginForm.tsx
@@ -2,8 +2,33 @@ import { Button } from '@/components/ui/button';
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { FormEvent, useState } from 'react';
 
 export const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('/api/v1/auth/login', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      });
+
+      console.log(response);
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+    } catch (err) {
+      console.log(`Error Occured - ${err}`);
+    }
+  };
+
   return (
     <Card className="w-full max-w-sm mx-auto p-6 bg-card">
       <CardHeader>
@@ -12,7 +37,7 @@ export const LoginForm = () => {
         </CardTitle>
       </CardHeader>
       <CardContent>
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={(e) => handleLogin(e)}>
           <div>
             <Label
               htmlFor="username"
@@ -25,6 +50,10 @@ export const LoginForm = () => {
               type="text"
               placeholder="Enter your username"
               className="mt-1"
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
             />
           </div>
           <div>
@@ -39,6 +68,10 @@ export const LoginForm = () => {
               type="password"
               placeholder="Enter your password"
               className="mt-1"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
             />
           </div>
           <Button className="w-full bg-primary hover:bg-primary-dark text-base text-primary-foreground">
